a4ca309a20a1aec47e674342a8f0ade7e32b23fd
Michał Kaźmierski <mi.kazmierski@gmail.com>
2022-06-26 18:47:49 +0200

    final_test

--- task_iv_ber/main.cpp
+++ task_iv_ber/main.cpp
@@ -1,51 +1,79 @@
 ﻿#include "pch.h"
-
+using namespace std;
 struct berResults
 {
-    double tot; //total number of bits
-    double err; //error bits (different bits)
-    float ber;  //ber calc result
-    clock_t t1; //calc start time
-    clock_t t2; //calc stop time
+    double tot;
+    double err;
+    float ber;
+    clock_t t1;
+    clock_t t2;
 };
 
 uint8_t hammingDistance(uint8_t n1, uint8_t n2);
-void createFile1(const std::string name, const int count, const char value);
-berResults calculateBer(std::string fpath1, std::string fpath2);
+void createFile1(const string name, const int count, const char value);
+berResults calculateBer(string fpath1, string fpath2);
 void printResult(berResults results);
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-    std::string fpath1; //path to file 1
-    std::string fpath2; //path to file 2
-    berResults results; 
+    string fpath1;
+    string fpath2;
+    berResults results;
 
-    openLog("log.log"); //create log.log file for logging purposes
-    if (argc != 3) //check the number of arguments
+    openLog("log.log");
+    if (argc != 3)
     {
-        saveLog("User did not provide path to the files.");
-        saveLog("Creating the test files for user...");
-        //test 1
-        createFile1("test1_file1.bin", 100, 0xFF); //1111 1111
-        createFile1("test1_file2.bin", 100, 0xFE); //1111 1110
-        //test 2
-        
-        /*
-            missing function to change 10 bits in the file
-        */
+        saveLog("Uzytkownik nie przedstawil sciezki do plikow.");
+        saveLog("Tworzenie plikow testowych przez uzytkownika...");
 
-        //test 3
-        //createFile1("test3_file1.bin",400000000,0x55); 
-        //createFile1("test3_file2.bin",400000000,0x50);
-        saveLog("Test files are prepared");
-        saveLog("Re-run with correct arguments ie: ./task_iv_ber.exe test1_file1.bin test1_file2.bin");
+
+
+        printf("\n\n Prosze wybrac test \n\n 1.Test 1: Przygotuj dwa identyczne pliki zawierajace binarnie zapisana wartosc 0x55. Kazdy plik po 100 Bajtow. \n\n 2.Test 2: Przygotuj dwa pliki kazdy po 100 bajtow. Pierwszy zawiera binarnie zapisana wartosc 0x55. Drugi plik rozni sie od pierwszego o dowolne 10 bitow. \n\n 3.Test 3: Przygotuj dwa pliki kazdy po 400 MB. Pierwszy zawiera binarnie zapisana wartosc 0x55. Drugi plik zawiera binarnie zapisana wartosc 0x50. \n\n ");
+        int wybor;
+        cin >> wybor;
+        switch (wybor) {
+
+        case 1:
+        {
+            //test 1 tworzymy takie same pliki z 0x55 
+
+            createFile1("test1_file1.bin", 100, 0x55);
+            createFile1("test1_file2.bin", 100, 0x55);
+            results = calculateBer("test1_file1.bin", "test1_file2.bin");
+            printResult(results);
+            break;
+        }
+        //test 2 pliki, ktore roznia sie 10 bajtami
+        case 2:
+        {
+            createFile1("test2_file1.bin", 100, 0x55);
+            createFile1("test2_file2.bin", 100, 0x5F);
+            results = calculateBer("test2_file1.bin", "test2_file2.bin");
+            printResult(results);
+            break;
+        }
+
+        case 3:
+        {
+            // test 3, 2 pliki po 400mb
+            createFile1("test3_file1.bin", 400000000, 0x55);
+            createFile1("test3_file2.bin", 400000000, 0x50);
+            results = calculateBer("test3_file1.bin", "test3_file2.bin");
+            printResult(results);
+            break;
+        }
+        }
+
+
+        saveLog("Pliki testowe gotowe.");
+        saveLog("Ponowne uruchamianie z odpowiednimi argumentami: ./task_iv_ber.exe test1_file1.bin test1_file2.bin");
     }
-    else //arguments are passed correctly
+    else
     {
         fpath1 = argv[1];
         fpath2 = argv[2];
 
-        saveLog("Processing test files");
+        saveLog("Trwa przetwarzanie plików testowych.");
         results = calculateBer(fpath1, fpath2);
         printResult(results);
     }
@@ -56,7 +84,7 @@ int main(int argc, char * argv[])
 
 uint8_t hammingDistance(uint8_t n1, uint8_t n2)
 {
-    uint8_t x = n1 ^ n2; // XOR
+    uint8_t x = n1 ^ n2;
     uint8_t setBits = 0;
     while (x > 0)
     {
@@ -66,20 +94,20 @@ uint8_t hammingDistance(uint8_t n1, uint8_t n2)
     return setBits;
 }
 
-void createFile1(const std::string name, const int count, const char value)
+void createFile1(const string name, const int count, const char value)
 {
-    std::fstream f;
-    f.open(name.c_str(), std::ios::binary | std::ios::out);
+    fstream f;
+    f.open(name.c_str(), ios::binary | ios::out);
     for (int i = 0; i < count; i++)
     {
-        f.write((char*)&value,1);
+        f.write((char*)&value, 1);
     }
     f.close();
 }
 
-berResults calculateBer(std::string fpath1, std::string fpath2)
+berResults calculateBer(string fpath1, string fpath2)
 {
-    std::fstream f1, f2; //handlers to files
+    fstream f1, f2;
     berResults results;
     results.t1 = 0;
     results.t2 = 0;
@@ -87,37 +115,37 @@ berResults calculateBer(std::string fpath1, std::string fpath2)
     results.err = 0;
     results.tot = 0;
 
-    saveLog("Calculating BER...");
-    f1.open(fpath1.c_str(), std::ios::binary | std::ios::in);
-    f2.open(fpath2.c_str(), std::ios::binary | std::ios::in);
+    saveLog("Obliczenia Bit Error Rate...");
+    f1.open(fpath1.c_str(), ios::binary | ios::in);
+    f2.open(fpath2.c_str(), ios::binary | ios::in);
     char a = 0x00;
     char b = 0x00;
     results.t1 = clock();
 
     while (!f1.eof())
     {
-        f1 >> a; //read 1 char from file 1
-        f2 >> b; //read 1 char from file 2
-        if (!f1.eof()) // till the end of the 1st file
+        f1 >> a;
+        f2 >> b;
+        if (!f1.eof())
         {
-            results.err += hammingDistance(a, b); //add to the .err the number of different bits
-            results.tot += 8; //add to the .tot the number of compared bits
+            results.err += hammingDistance(a, b);
+            results.tot += 8;
         }
     }
 
-    results.ber = (float)results.err / results.tot; // calculate ber
-    results.t2 = clock(); 
-    saveLog("BER calculations are done");
-    return results; //return structure with all results
+    results.ber = (float)results.err / results.tot;
+    results.t2 = clock();
+    saveLog("Obliczenia Bit Error Rate zakończone.");
+    return results;
 }
 
 void printResult(berResults results)
 {
-    std::stringstream message;
-    message << "Results are: " << std::endl;
-    message << "BER: " << results.ber << std::endl;
-    message << "Tot: " << results.tot << std::endl;
-    message << "Err: " << results.err << std::endl;
-    message << "Calc time: " << ((float)results.t2 - results.t1) / CLOCKS_PER_SEC << " sec " << std::endl;
+    stringstream message;
+    message << "Results are: " << endl;
+    message << "BER: " << results.ber << endl;
+    message << "Tot: " << results.tot << endl;
+    message << "Err: " << results.err << endl;
+    message << "Calc time: " << ((float)results.t2 - results.t1) / CLOCKS_PER_SEC << " sec " << endl;
     saveLog(message.str());
 }
